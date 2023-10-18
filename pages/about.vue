4bd3542fd1e8afd83c5ad1c50e12e4348a75c3de
JrzenonDev <jrobertoonb@gmail.com>
2023-10-18 18:47:55 -0300

    feat: init pages

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, fugiat
+      provident. Fuga officia, sit harum perspiciatis nulla molestias, neque
+      quod dolores at maiores nihil sed beatae iure a enim. Soluta!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
